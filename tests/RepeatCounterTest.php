9ff686565416e6cc28a85bc6c514a81ecf8f427f
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-09-16 10:22:31 -0400

    add placeholder text for specific tests to run

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -4,7 +4,65 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
+        function test_WordBlank() { // Spec #1
 
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+
+        }
+
+        function test_StringofWordsBlank() { // Spec #2
+
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+
+        }
+
+        function test_WordhasSpace() { // Spec #3
+
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+
+        }
+
+        function test_OnlyOneMatch() { // Spec #4
+
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+
+        }
+
+        function test_MultipleMatch() { // Spec #5
+
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+
+        }
+
+        function test_NoMatch() { // Spec #6
+
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+
+        }
 
     }
 
