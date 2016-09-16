4d650ae812708ea397cc2fe9d1870df65c630f6a
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-09-16 10:31:58 -0400

    add ARRANGE - ACT - ASSERT variables

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -7,62 +7,98 @@
         function test_WordBlank() { // Spec #1
 
             //ARRANGE
+            $input_word_string_to_check =
+            $input_word_to_check =
+            $expected_output =
+            $repeat_counter_instance = new RepeatCounter($input_word_string_to_check, $input_word_to_check);
 
             //ACT
+            $test_result = $repeat_counter_instance->countRepeats();
 
             //ASSERT
+            $this->assertEquals($expected_output, $test_result);
 
         }
 
-        function test_StringofWordsBlank() { // Spec #2
+        // function test_StringofWordsBlank() { // Spec #2
+        //
+        //     //ARRANGE
+        //     $input_word_string_to_check =
+        //     $input_word_to_check =
+        //     $expected_output =
+        //     $repeat_counter_instance = new RepeatCounter($input_word_string_to_check, $input_word_to_check);
+        //
+        //     //ACT
+        //     $test_result = $repeat_counter_instance->countRepeats();
+        //
+        //     //ASSERT
+        //     $this->assertEquals($expected_output, $test_result);
+        //
+        // }
 
-            //ARRANGE
+        // function test_WordhasSpace() { // Spec #3
+        //
+        //     //ARRANGE
+        //     $input_word_string_to_check =
+        //     $input_word_to_check =
+        //     $expected_output =
+        //     $repeat_counter_instance = new RepeatCounter($input_word_string_to_check, $input_word_to_check);
+        //
+        //     //ACT
+        //     $test_result = $repeat_counter_instance->countRepeats();
+        //
+        //     //ASSERT
+        //     $this->assertEquals($expected_output, $test_result);
+        //
+        // }
 
-            //ACT
+        // function test_OnlyOneMatch() { // Spec #4
+        //
+        //     //ARRANGE
+        //     $input_word_string_to_check =
+        //     $input_word_to_check =
+        //     $expected_output =
+        //     $repeat_counter_instance = new RepeatCounter($input_word_string_to_check, $input_word_to_check);
+        //
+        //     //ACT
+        //     $test_result = $repeat_counter_instance->countRepeats();
+        //
+        //     //ASSERT
+        //     $this->assertEquals($expected_output, $test_result);
+        //
+        // }
 
-            //ASSERT
+        // function test_MultipleMatch() { // Spec #5
+        //
+        //     //ARRANGE
+        //     $input_word_string_to_check =
+        //     $input_word_to_check =
+        //     $expected_output =
+        //     $repeat_counter_instance = new RepeatCounter($input_word_string_to_check, $input_word_to_check);
+        //
+        //     //ACT
+        //     $test_result = $repeat_counter_instance->countRepeats();
+        //
+        //     //ASSERT
+        //     $this->assertEquals($expected_output, $test_result);
+        //
+        // }
 
-        }
-
-        function test_WordhasSpace() { // Spec #3
-
-            //ARRANGE
-
-            //ACT
-
-            //ASSERT
-
-        }
-
-        function test_OnlyOneMatch() { // Spec #4
-
-            //ARRANGE
-
-            //ACT
-
-            //ASSERT
-
-        }
-
-        function test_MultipleMatch() { // Spec #5
-
-            //ARRANGE
-
-            //ACT
-
-            //ASSERT
-
-        }
-
-        function test_NoMatch() { // Spec #6
-
-            //ARRANGE
-
-            //ACT
-
-            //ASSERT
-
-        }
+        // function test_NoMatch() { // Spec #6
+        //
+        //     //ARRANGE
+        //     $input_word_string_to_check =
+        //     $input_word_to_check =
+        //     $expected_output =
+        //     $repeat_counter_instance = new RepeatCounter($input_word_string_to_check, $input_word_to_check);
+        //
+        //     //ACT
+        //     $test_result = $repeat_counter_instance->countRepeats();
+        //
+        //     //ASSERT
+        //     $this->assertEquals($expected_output, $test_result);
+        //
+        // }
 
     }
 
