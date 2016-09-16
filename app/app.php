1a8a843be2ea636281e4a66174c858800f14d7b2
Lisa MacCarrigan <Lisa.MacCarrigan@gmail.com>
2016-09-16 15:12:40 -0400

    add search result twig template, add route to search result page

--- app/app.php
+++ app/app.php
@@ -10,6 +10,15 @@
         return $app["twig"]->render("form.html.twig");
     });
 
+    $app->get("/search_word", function() use($app) {
+        $search_sentence = $_GET["input-sentence"];
+        $search_word = $_GET["input-word"];
+        $new_word_repeat_counter = new RepeatCounter($search_sentence, $search_word);
+        $count = $new_word_repeat_counter->countRepeats();
+
+        return $app["twig"]->render("search_result.html.twig", array("word_searched" => $search_word, "sentence_searched" => $search_sentence, "word_count" => $count));
+    });
+
     return $app;
 
 ?>
